8383341bd58bb47e5c8b9629c73b38208291a4cc
Fabio <fabio.ottaviani@gmail.com>
2017-04-07 14:25:48 +0200

    Transform Vendors

--- js/main.js
+++ js/main.js
@@ -5,33 +5,50 @@ Website by Websolute
 
 
 /*--------------------------------------------------
-DOC READY
+Transform Vendor
 --------------------------------------------------*/
+var transformProp = (function () {
+    var testEl = document.createElement('div');
+    if (testEl.style.transform == null) {
+        var vendors = ['Webkit', 'Moz', 'ms'];
+        for (var vendor in vendors) {
+            if (testEl.style[vendors[vendor] + 'Transform'] !== undefined) {
+                return vendors[vendor] + 'Transform';
+            }
+        }
+    }
+    return 'transform';
+})();
+
+
+
+/*--------------------------------------------------
+Scrollbar Listener
+--------------------------------------------------*/
+var scrollbar = Scrollbar.init(document.getElementById('myScrollbar'), {
+    overscrollEffect: 'bounce'
+});
 var parallax = document.querySelector('#parallax');
 var parallax2 = document.querySelector('#parallax2 img');
 var height = window.innerHeight;
 
-var scrollbar = Scrollbar.init(document.getElementById('myScrollbar'), {
-    overscrollEffect: 'bounce'
-});
-
-var i = 0;
-
-
 scrollbar.addListener(function (status) {
-    console.log(scrollbar.isVisible(parallax));
+    console.log(scrollbar.isVisible(parallax), status.offset.y);
+
+    // parallax in visible
     if (scrollbar.isVisible(parallax)) {
-        i = parallax.getBoundingClientRect().top - height / 2;
+        i = parallax.getBoundingClientRect().top - height / 4;
         y = parallax2.getBoundingClientRect().top - height / 4;
-        parallax.style.webkitTransform = 'translateY(' + i * 0.2 + 'px)';
-        //parallax.style.webkitTransform = 'translate(' + i * 0.2 + 'px, ' + i * 0.2 + 'px)';
-        parallax2.style.webkitTransform = 'translateY(' + y * 0.1 + 'px)';
+        parallax.style[transformProp] = 'translateY(' + i * 0.2 + 'px)';
+        parallax2.style[transformProp] = 'translateY(' + y * 0.1 + 'px)';
     }
 
-    // , status.offset.y, parallax.getBoundingClientRect()
 });
 
 
+/*--------------------------------------------------
+DOC READY
+--------------------------------------------------*/
 $(function () {
 });
 
@@ -40,5 +57,4 @@ $(function () {
 WIN LOAD
 --------------------------------------------------*/
 $(window).on('load', function () {
-    scrollbar.update();
 });
